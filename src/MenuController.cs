3e5795369915c2dfdb6a718c2da1978b5ed2aaa1
B.Giller <101624498@student.swin.edu.au>
2019-05-09 13:37:06 +1000

    Change menu button colours

--- src/MenuController.cs
+++ src/MenuController.cs
@@ -44,8 +44,9 @@ static class MenuController
 	private const int MENU_TOP = 575;
 	private const int MENU_LEFT = 30;
 	private const int MENU_GAP = 0;
-	private const int BUTTON_WIDTH = 75;
-	private const int BUTTON_HEIGHT = 15;
+	//Alter button sizes to be slightly larger.
+	private const int BUTTON_WIDTH = 80;
+	private const int BUTTON_HEIGHT = 20;
 	private const int BUTTON_SEP = BUTTON_WIDTH + MENU_GAP;
 
 	private const int TEXT_OFFSET = 0;
@@ -67,9 +68,10 @@ static class MenuController
 	private const int GAME_MENU_SURRENDER_BUTTON = 1;
 
 	private const int GAME_MENU_QUIT_BUTTON = 2;
-	private static readonly Color MENU_COLOR = SwinGame.RGBAColor(2, 167, 252, 255);
-
-	private static readonly Color HIGHLIGHT_COLOR = SwinGame.RGBAColor(1, 57, 86, 255);
+	//Alteration of the colours to be much more readable for others. Aqua.
+	private static readonly Color MENU_COLOR = SwinGame.RGBAColor(0, 255, 255, 255);
+	//Magenta / Pink colour for the rectanglar highlight.
+	private static readonly Color HIGHLIGHT_COLOR = SwinGame.RGBAColor(255, 0, 255, 255);
 	/// <summary>
 	/// Handles the processing of user input when the main menu is showing
 	/// </summary>
